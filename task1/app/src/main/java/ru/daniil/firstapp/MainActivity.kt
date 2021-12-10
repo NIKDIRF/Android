7edcf6705a27b00f5419ea32ecdf763868e22507
NIKDIRF <fridkin95@gmail.com>
2021-12-10 11:34:12 +0300

    no style buttons and clickable numbers

--- task1/app/src/main/java/ru/daniil/firstapp/MainActivity.kt
+++ task1/app/src/main/java/ru/daniil/firstapp/MainActivity.kt
@@ -3,7 +3,10 @@ package ru.daniil.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
 import ru.daniil.firstapp.databinding.ActivityMainBinding
+import ru.daniil.firstapp.databinding.ActivityMainBinding.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,17 +14,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putParcelable(KEY,binding.textViewResult.text.toString())
+        outState.putString(KEY,binding.textViewResult.text.toString())
         super.onSaveInstanceState(outState)
     }
 
     companion object {
         const val KEY = "result"
+        const val DEFAULT = "…|"
+        const val NUMBERS = setOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+    }
+
+    fun buttonClicked(view: View) {
+        val buttonChar = (view as Button).text
+        if (binding.textViewResult.text == DEFAULT ) {
+            if (buttonChar in NUMBERS) {
+                binding.textViewResult.text = buttonChar
+            }
+        } else {
+            binding.textViewResult.append(buttonChar)
+        }
     }
 
 }
